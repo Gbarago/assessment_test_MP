86c735cd983da8947933c3c0d33543cb19760b81
Abiodun Oni <abiodun.oni@bankly.ng>
2024-11-01 08:49:42 +0100

    mapp custom markers

--- lib/UI/screens/search_screen.dart
+++ lib/UI/screens/search_screen.dart
@@ -1,11 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter/services.dart' show Uint8List, rootBundle;
 import 'package:real_estate_abiodun/UI/screens/widgets/search_map_widgets/map-searchbar_widget.dart';
 import 'package:real_estate_abiodun/UI/screens/widgets/search_map_widgets/markers.dart';
 import 'package:real_estate_abiodun/utils/estensions.dart';
-
+import 'package:real_estate_abiodun/utils/theme.dart';
+import 'package:screenshot/screenshot.dart';
 import 'widgets/search_map_widgets/map_dialogue.dart';
 import 'widgets/search_map_widgets/map_fab_right.dart';
 
@@ -17,102 +20,244 @@ class SearchScreenTab extends StatefulWidget {
 }
 
 class _SearchScreenTabState extends State<SearchScreenTab>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
   late GoogleMapController mapController;
   late AnimationController _animationController;
-
-  final LatLng _center =
-      const LatLng(37.7749, -122.4194); // Initial map center (San Francisco)
-  final Set<Marker> _markers = {};
-  String? _mapStyle; // Variable to hold the dark map style
+  final LatLng _center = const LatLng(37.7749, -122.4194); // San Francisco
+  // Set<Marker> _markers = {};
+  int currentFrameIndex = 0;
+  String? _mapStyle;
+  final ScreenshotController screenshotController = ScreenshotController();
+  Timer? _timer;
+  //List<Uint8List> markerFrames = [];
   bool _isExpanded = true;
+
+  // Uint8List? expandedMarker;
+  // Uint8List? collapsedMarker;
+  // final List<LatLng> markerPositions = const [
+  //   LatLng(37.7749, -122.4193), // Center
+  //   LatLng(37.7849, -122.4194), // North
+  //   LatLng(37.7649, -122.4194), // South
+  //   LatLng(37.7749, -122.4294), // West
+  //   LatLng(37.7749, -122.4094), // East
+  //   LatLng(37.7849, -122.4294), // Northwest
+  //   LatLng(37.7649, -122.4094), // Southeast
+  // ];
+
+  Future<Uint8List> getBytesFromWidget(
+    Widget widget, {
+    required ScreenshotController screenshotController,
+    required int width,
+    //!overlayExpanded ? 35 : 75,  ,
+    required int height,
+  }) async {
+    final Uint8List? imageBytes = await screenshotController.captureFromWidget(
+      MediaQuery(
+        data: MediaQueryData(size: Size(width.toDouble(), height.toDouble())),
+        child: Directionality(
+          textDirection: TextDirection.ltr,
+          child: SizedBox(
+            width: width.toDouble(),
+            height: height.toDouble(),
+            child: widget,
+          ),
+        ),
+      ),
+      pixelRatio: 1.5,
+    );
+    return imageBytes!;
+  }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+
+  //   // Load both expanded and collapsed marker frames once
+  //   _loadMarkerFrames();
+
+  //   _animationController = AnimationController(
+  //     vsync: this,
+  //     duration: 700.ms,
+  //     reverseDuration: 500.ms,
+  //   );
+
+  //   _animationController.addStatusListener((listener) {
+  //     // if (listener == AnimationStatus.completed ||
+  //     //     listener == AnimationStatus.dismissed) {
+  //     setState(() {
+  //       _isExpanded = !_isExpanded;
+  //       _loadMarkerFrames().then((_) => _toggleExpansion());
+  //     });
+  //     //  }
+  //   });
+
+  //   // Optional: Load the map style
+  //   rootBundle.loadString('assets/map_styles/dark_map.json').then((style) {
+  //     setState(() {
+  //       _mapStyle = style;
+  //     });
+  //   });
+  // }
+
+  // Future<void> _loadMarkerFrames() async {
+  //   expandedMarker = await _generateMarkerFrame(true);
+  //   collapsedMarker = await _generateMarkerFrame(false);
+  //   _addMarkers(); // Add markers once loaded
+  // }
+
+  // Future<Uint8List> _generateMarkerFrame(bool isExpanded) async {
+  //   return await getBytesFromWidget(
+  //     ListOfMarkersWidget(
+  //       isExpanded: isExpanded,
+  //       color: AppTheme.primaryColor,
+  //     ),
+  //     screenshotController: screenshotController,
+  //     width: isExpanded ? 75 : 35,
+  //     height: 35,
+  //   );
+  // }
+
+  // void _toggleExpansion() {
+  //   setState(() {
+  //     _isExpanded = !_isExpanded;
+  //     _addMarkers(); // Update markers instantly when _isExpanded changes
+  //   });
+  // }
+
+  // void _addMarkers() {
+  //   final Uint8List markerIcon =
+  //       _isExpanded ? expandedMarker! : collapsedMarker!;
+  //   _markers = markerPositions.map((position) {
+  //     return Marker(
+  //       markerId: MarkerId(position.toString()),
+  //       position: position,
+  //       icon: BitmapDescriptor.bytes(markerIcon),
+  //     );
+  //   }).toSet();
+  // }
+
+  Uint8List? expandedMarker;
+  Uint8List? collapsedMarker;
+  Set<Marker> _markers = {};
+  final List<LatLng> markerPositions = [
+    LatLng(37.7749, -122.4193),
+    LatLng(37.7849, -122.4194),
+    LatLng(37.7649, -122.4194),
+    LatLng(37.7749, -122.4294),
+    LatLng(37.7749, -122.4094),
+    LatLng(37.7849, -122.4294),
+    LatLng(37.7649, -122.4094),
+  ];
+
+  // final ScreenshotController screenshotController = ScreenshotController();
 
   @override
   void initState() {
     super.initState();
 
     _animationController = AnimationController(
-        vsync: this, duration: 700.ms, reverseDuration: 500.ms);
+      vsync: this,
+      duration: 700.ms,
+      reverseDuration: 500.ms,
+    );
+
     _animationController.addStatusListener((listener) {
-      if (listener == AnimationStatus.dismissed) {
-        _setExpandedFalse();
-      } else {
-        _setExpandedTrue();
+      _loadMarkerFrames();
+
+      if (listener == AnimationStatus.completed ||
+          listener == AnimationStatus.dismissed) {
+        setState(() {
+          _isExpanded = !_isExpanded;
+          // _loadMarkerFrames().then((_) => _updateMarkers());
+        });
       }
     });
 
-    // Load the dark map style from assets
-    rootBundle.loadString('assets/map_styles/dark_map.json').then((string) {
+    _loadMarkerFrames();
+    // Optionally: Load the map style
+
+    rootBundle.loadString('assets/map_styles/dark_map.json').then((style) {
       setState(() {
-        _mapStyle = string; // Update the state with the loaded style
+        _mapStyle = style;
       });
     });
-
-    // Add markers with chat bubble widgets
-    _addMarkers();
   }
 
-  void _setExpandedFalse() {
+  Future<void> _loadMarkerFrames() async {
+    expandedMarker = await _generateMarkerFrame(true);
+    collapsedMarker = await _generateMarkerFrame(false);
+    _addMarkers(); // Add markers after loading frames
+  }
+
+  // Future<Uint8List> _generateMarkerFrame(bool isExpanded) async {
+  //   return await getBytesFromWidget(
+  //     ListOfMarkersWidget(
+  //       isExpanded: isExpanded,
+  //       color: AppTheme.primaryColor,
+  //     ),
+
+  //     // Container(
+  //     //   width: 150,
+  //     //   height: 50,
+  //     //   color: AppTheme.primaryColor,
+  //     // ),
+  //     screenshotController: screenshotController,
+  //     width: isExpanded ? 75 : 35,
+  //     height: 35,
+  //   );
+  // }
+
+  Future<Uint8List> _generateMarkerFrame(bool isExpanded) async {
+    final AnimationController animationController = AnimationController(
+      vsync: this, // Requires this mixin
+      duration: const Duration(milliseconds: 300),
+    );
+    final Animation<double> animation =
+        Tween(begin: 1.0, end: isExpanded ? 1.2 : 0.8)
+            .animate(animationController);
+
+    animationController.forward();
+
+    // Capture widget after animation completes
+    await Future.delayed(animationController.duration!);
+
+    final Uint8List markerFrame = await getBytesFromWidget(
+      ListOfMarkersWidget(
+        isExpanded: isExpanded,
+        color: AppTheme.primaryColor,
+        // animation: animation,
+      ),
+      screenshotController: screenshotController,
+      width: isExpanded ? 75 : 35,
+      height: 35,
+    );
+
+    animationController.dispose();
+    return markerFrame;
+  }
+
+  void _toggleExpansion() async {
     setState(() {
-      _isExpanded = false;
+      _isExpanded = !_isExpanded;
+      _markers.clear(); // Clear markers to refresh
     });
+    // Small delay to ensure markers are cleared before re-adding
+    await Future.delayed(const Duration(milliseconds: 10));
+    _addMarkers(); // Add markers with updated icon
   }
 
-  void _setExpandedTrue() {
-    setState(() {
-      _isExpanded = true;
-    });
-  }
-
-  // Function to add 6 markers
   void _addMarkers() {
-    _markers.add(
-      Marker(
-        markerId: MarkerId('marker_1'),
-        position: LatLng(37.7769, -122.4194), // Position for marker 1
-        infoWindow: InfoWindow(
-          title: 'Marker 1',
-          snippet: 'This is a custom chat bubble!',
-        ),
-        onTap: () {
-          _showChatBubble(context, 'This is marker 1!');
-        },
-      ),
-    );
-
-    // Adding 5 more markers
-    for (int i = 2; i <= 6; i++) {
-      _markers.add(
-        Marker(
-          markerId: MarkerId('marker_$i'),
-          position: LatLng(37.7749 + i * 0.01,
-              -122.4194 - i * 0.04), // Adjusted position for markers 2-6
-          infoWindow: InfoWindow(
-            title: 'Marker $i',
-            snippet: 'This is a custom chat bubble!',
-          ),
-          // onTap: () {
-          //   _showChatBubble(context, 'This is marker $i!');
-          // },
-        ),
-      );
-    }
-  }
-
-  // Function to display the chat bubble dialog
-  void _showChatBubble(BuildContext context, String message) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        content: Text(message),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: Text('Close'),
-          ),
-        ],
-      ),
-    );
+    final Uint8List markerIcon =
+        _isExpanded ? expandedMarker! : collapsedMarker!;
+    setState(() {
+      _markers = markerPositions.map((position) {
+        return Marker(
+          markerId: MarkerId(position.toString()),
+          position: position,
+          icon: BitmapDescriptor.bytes(markerIcon),
+        );
+      }).toSet();
+    });
   }
 
   @override
@@ -122,16 +267,27 @@ class _SearchScreenTabState extends State<SearchScreenTab>
         fit: StackFit.expand,
         children: [
           GoogleMap(
-            onMapCreated: (GoogleMapController controller) {
-              mapController = controller;
-            },
+            markers: _markers,
+            // onMapCreated: (GoogleMapController controller) {
+            //   mapController = controller;
+            // },
+            onMapCreated: (controller) => mapController = controller,
+
             initialCameraPosition: CameraPosition(
               target: _center,
-              zoom: 11.0,
+              zoom: 14.0,
             ),
             style: _mapStyle,
           ),
           const ExtendedRightFabBTN(),
+          // Positioned(
+          //   left: 30,
+          //   bottom: context.sizeHeight(0.11),
+          //   child: OverlayDialog(
+          //     animationController: _animationController,
+          //   ),
+          //  ),
+
           Positioned(
             left: 30,
             bottom: context.sizeHeight(0.11),
@@ -139,10 +295,110 @@ class _SearchScreenTabState extends State<SearchScreenTab>
               animationController: _animationController,
             ),
           ),
+
           const MapSearchBarWidget(),
-          ListOfMarkersWidget(isExpanded: _isExpanded)
         ],
       ),
     );
   }
 }
+
+
+
+
+
+
+
+ // Future<Uint8List> getBytesFromWidget(
+  //   Widget widget, {
+  //   required ScreenshotController screenshotController,
+  //   required int width,
+  //   //!overlayExpanded ? 35 : 75,  ,
+  //   required int height,
+  // }) async {
+  //   final Uint8List? imageBytes = await screenshotController.captureFromWidget(
+  //     MediaQuery(
+  //       data: MediaQueryData(size: Size(width.toDouble(), height.toDouble())),
+  //       child: Directionality(
+  //         textDirection: TextDirection.ltr,
+  //         child: SizedBox(
+  //           width: width.toDouble(),
+  //           height: height.toDouble(),
+  //           child: widget,
+  //         ),
+  //       ),
+  //     ),
+  //     pixelRatio: 1.5,
+  //   );
+  //   return imageBytes!;
+  // }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+
+  //   // Load marker frames only when the _isExpanded changes
+  //   _loadMarkerFrames().then((_) => _updateMarkers());
+
+  //   _animationController = AnimationController(
+  //     vsync: this,
+  //     duration: 700.ms,
+  //     reverseDuration: 500.ms,
+  //   );
+
+  //   _animationController.addStatusListener((listener) {
+  //     if (listener == AnimationStatus.completed ||
+  //         listener == AnimationStatus.dismissed) {
+  //       setState(() {
+  //         _isExpanded = !_isExpanded;
+  //         _loadMarkerFrames().then((_) => _updateMarkers());
+  //       });
+  //     }
+  //   });
+
+  //   // Load the dark map style from assets
+  //   rootBundle.loadString('assets/map_styles/dark_map.json').then((string) {
+  //     setState(() {
+  //       _mapStyle = string;
+  //     });
+  //   });
+  // }
+
+  // Future<void> _loadMarkerFrames() async {
+  //   markerFrames.clear();
+  //   for (int i = 0; i < 2; i++) {
+  //     final Uint8List markerFrame = await getBytesFromWidget(
+  //       ListOfMarkersWidget(
+  //         isExpanded: _isExpanded,
+  //         color: AppTheme.primaryColor,
+  //       ),
+  //       screenshotController: screenshotController,
+  //       width: _isExpanded ? 75 : 35,
+  //       height: 35,
+  //     );
+  //     markerFrames.add(markerFrame);
+  //   }
+  // }
+
+  // // Function to update markers with the latest frame only once
+  // void _updateMarkers() {
+  //   final List<LatLng> markerPositions = [
+  //     LatLng(37.7749, -122.4193), // Center
+  //     LatLng(37.7849, -122.4194), // North
+  //     LatLng(37.7649, -122.4194), // South
+  //     LatLng(37.7749, -122.4294), // West
+  //     LatLng(37.7749, -122.4094), // East
+  //     LatLng(37.7849, -122.4294), // Northwest
+  //     LatLng(37.7649, -122.4094), // Southeast
+  //   ];
+
+  //   setState(() {
+  //     _markers = markerPositions.map((position) {
+  //       return Marker(
+  //         markerId: MarkerId(position.toString()),
+  //         position: position,
+  //         icon: BitmapDescriptor.bytes(markerFrames.last),
+  //       );
+  //     }).toSet();
+  //   });
+  // }
